50cd04a989989a02ab615bb96298758c3a5696fb
Adrian Gaudebert <adrian@gaudebert.fr>
2012-03-18 21:46:22 +0100

    Improved general UI, added icons and more information. Closes #1

--- js/gui.js
+++ js/gui.js
@@ -7,4 +7,31 @@ $(document).ready(function() {
         $('.unit-icon').removeClass('selected-unit');
         $(this).addClass('selected-unit');
     });
+
+    $('#unit').simpletip({
+        'content': 'A basic unit, can hit ennemies on all cells around it. Moves one case forward each turn, middle attack strength, good defense.',
+        'fixed': false
+    });
+    $('#side-unit').simpletip({
+        'content': 'A unit with a lower defense and that cannot hit units in front of it, but that can hit units 2 cells away on each side.',
+        'fixed': false
+    });
+    $('#heavy-unit').simpletip({
+        'content': 'A heavy unit with a very good defense but lower attack power. It cannot move.',
+        'fixed': false
+    });
+
+    REACH.updateUI = function(turn) {
+        var activePlayer = turn.getActivePlayer(),
+            inactivePlayer = turn.getInactivePlayer(),
+            active = $('#player-' + activePlayer.playerNumber),
+            inactive = $('#player-' + inactivePlayer.playerNumber);
+
+        active.addClass('active');
+        inactive.removeClass('active');
+
+        active.children('.player-score').children('span').text(activePlayer.points);
+        inactive.children('.player-score').children('span').text(inactivePlayer.points);
+    };
+
 });
